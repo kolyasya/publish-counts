61bb5825a587889e30d8b62cc0dacd0c23ab7b25
Bryan Tafel <bryantafel@bryans-mbp.lan>
2017-08-17 20:17:18 -0400

    Bump version 0.9.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "btafel:publish-counts",
   summary: "Publish the count of a cursor, in real time or with pulling time",
-  version: "0.9.1",
+  version: "0.9.2",
   git: "https://github.com/percolatestudio/publish-counts.git"
 });
 
